9e2764324a0e55ef1d8b7c9ac2d6595ae4ed5846
Michael Haglund <michael.haglund@life.church>
2020-11-10 18:46:59 -0600

    init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {},
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          default: '#2993E5',
+          lighter: '#E5F2FC',
+          darker: '#2B6A9C'
+        }
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
